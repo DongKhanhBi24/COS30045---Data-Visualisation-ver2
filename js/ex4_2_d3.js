67f85667c372de98817e752be801ee2d82e5e7aa
Nguyen Bi <binguyen@192.168.1.232>
2026-03-04 22:30:18 +0700

    Excercise 4.2: D3 testing

---
+++ js/ex4_2_d3.js
@@ -0,0 +1,14 @@
+d3.select("h1")
+  .style("color", "green");
+
+d3.select("div")
+  .append("p")
+  .text("Purchasing a low energy consumption TV will help with your energy bills!");
+
+d3.select("svg") 
+  .append("rect")
+  .attr("x", 100)
+  .attr("y", 100)
+  .attr("width", 200)
+  .attr("height", 350)
+  .attr("fill", "green");
