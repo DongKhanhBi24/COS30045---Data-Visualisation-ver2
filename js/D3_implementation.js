06d5ca11d8548aa1725cc54354d4b0cc6493f941
Nguyen Bi <binguyen@Bis-Mac.local>
2026-03-05 15:16:15 +0700

    Exercise 4.7 Adding labels

--- js/D3_implementation.js
+++ js/D3_implementation.js
@@ -29,6 +29,7 @@ d3.csv("../data/count_models_of_brands.csv", d => {
 
 const createBarChart = (data) => {
   const leftPadding = 70;  // labels
+  const chartheight = 1450; 
 
   const xScale = d3.scaleLinear() 
   .domain([0, d3.max(data, d => d.count)])
@@ -36,17 +37,42 @@ const createBarChart = (data) => {
 
   const yScale = d3.scaleBand() 
   .domain(data.map(d => d.brand))
-  .range([0, 500])
+  .range([0, chartheight])
   .padding(0.3);
 
-  svg
-    .selectAll("rect.bar")         
-    .data(data)
-    .join("rect")
-    .attr("class", d => `bar bar-${d.count}`)
-    .attr("x", leftPadding)
-    .attr("y", d => 50 + yScale(d.brand))
-    .attr("height", yScale.bandwidth())
-    .attr("width", d => xScale(d.count))    
-    .attr("fill", "steelblue");
+  const barAndLabel = svg
+  .selectAll("g.bar-and-label")
+  .data(data)
+  .join("g")
+  .attr("class", "bar-and-label")
+  .attr("transform", d => `translate(0, ${50 + yScale(d.brand)})`);
+
+  barAndLabel
+  .append("rect")
+  .attr("class", d => `bar bar-${d.count}`)
+  .attr("x", leftPadding)
+  .attr("y", 0)
+  .attr("height", yScale.bandwidth())
+  .attr("width", d => xScale(d.count))
+  .attr("fill", "green");
+
+  barAndLabel
+  .append("text")
+  .text(d => d.brand)
+  .attr("x", leftPadding - 5)
+  .attr("y", yScale.bandwidth() / 2)
+  .attr("dominant-baseline", "middle")
+  .attr("text-anchor", "end")
+  .style("font-family", "sans-serif")
+  .style("font-size", "6px");
+
+  barAndLabel
+  .append("text")
+  .text(d => d.count)
+  .attr("x", d => leftPadding + xScale(d.count) + 4)
+  .attr("y", yScale.bandwidth() / 2)
+  .attr("dominant-baseline", "middle")
+  .style("font-family", "sans-serif")
+  .style("font-size", "6px");
 };
+
